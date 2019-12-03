64efa9d4c13a3e885f681cc1e16be7a028aa9f81
lkyadav123 <laxmi.kant@srslive.in>
2019-12-03 16:26:07 +0530

    First commit

---
+++ src/main/java/com/practice/DesignPattern/DesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.practice.DesignPattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternApplication.class, args);
+	}
+
+}
